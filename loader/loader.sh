508c7fbf396f4b556289cbdcb08ca108b901fa05
Даниил Ешенко <91437231+Yawllen@users.noreply.github.com>
2025-11-08 10:18:04 +0300

    Update loader.sh

--- loader/loader.sh
+++ loader/loader.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
-TARGET_DIR="/home/pi/treed"
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-mkdir -p "$TARGET_DIR"
-rsync -a --delete --exclude '.git' --exclude '.github' --exclude '.staging' --exclude 'loader' --exclude 'moonraker.conf' --exclude 'KlipperScreen.conf' "$REPO_DIR/" "$TARGET_DIR/"
-systemctl restart moonraker.service || true
-systemctl restart klipper.service || true
-systemctl restart KlipperScreen.service || true
+sudo install -d -m 755 /usr/share/plymouth/themes/treed
+sudo cp -a "$REPO_DIR/loader/plymouth/treed/treed.plymouth" /usr/share/plymouth/themes/treed/
+sudo cp -a "$REPO_DIR/loader/plymouth/treed/treed.script" /usr/share/plymouth/themes/treed/
+sudo cp -a "$REPO_DIR/loader/plymouth/treed/watermark.png" /usr/share/plymouth/themes/treed/ 2>/dev/null || true
+sudo chown root:root /usr/share/plymouth/themes/treed/*
+sudo chmod 0644 /usr/share/plymouth/themes/treed/*
+sudo plymouth-set-default-theme -R treed
+sudo install -d -m 755 /etc/systemd/system/KlipperScreen.service.d
+sudo cp -a "$REPO_DIR/loader/systemd/KlipperScreen.service.d/override.conf" /etc/systemd/system/KlipperScreen.service.d/override.conf
+sudo systemctl daemon-reload
+sudo install -d -m 755 /home/pi/printer_data/config/.theme
+sudo rsync -a --delete "$REPO_DIR/mainsail/.theme/" /home/pi/printer_data/config/.theme/
+sudo chown -R pi:$(id -gn pi) /home/pi/printer_data/config/.theme
